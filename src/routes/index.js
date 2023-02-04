7974a9106a96ca04787a94145ee8a4814fd175b7
donny <doniwahyu14@gmail.com>
2023-02-04 13:47:32 +0700

    add tanggal folder

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const Router = express.Router();
 
 const karyawanRoutes = require("../moduls/karyawan/karyawanRoutes");
+// const tanggalRoutes = require("../moduls/tanggal/tanggalRoutes");
 
 Router.use("/karyawan", karyawanRoutes);
+// Router.use("/tanggal", tanggalRoutes);
 
 module.exports = Router;
